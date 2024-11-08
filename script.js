d687221b55403f7c612edbba4c393c2dedf5f587
Marcelo Meneses <marcelmenesess@gmail.com>
2024-11-08 14:31:45 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('.button-github').onclick = function() {
+    window.open('https://github.com/MenesesMG', '_blank');
+};
+
+document.querySelector('.button-frontend-mentor').onclick = function() {
+    window.open('https://www.frontendmentor.io/profile/MenesesMG','_blank');
+};
+
+document.querySelector('.button-linkedin').onclick = function() {
+    window.open('https://www.linkedin.com/', '_blank');
+};
+
+document.querySelector('.button-twitter').onclick = function() {
+    window.open('https://www.x.com/', '_blank');
+};
+
+document.querySelector('.button-instagram').onclick = function() {
+    window.open('https://www.instagram.com/', '_blank');
+};
